934513b6988b9fc457352fc93e915798dcfb756c
Jaesin Mulenex <jaesin@no-reply.gradientsky.com>
2016-08-22 23:37:05 -0700

    [PHPCS] Mostly comment updates.

--- src/EncryptionServiceInterface.php
+++ src/EncryptionServiceInterface.php
@@ -10,30 +10,39 @@ namespace Drupal\encryption;
 interface EncryptionServiceInterface {
 
   /**
-   * Encrypt a value using the encryption key from settings.php
+   * Encrypt a value using the encryption key from settings.php.
    *
-   * @param $value
+   * @param string $value
    *   The value tobe encrypted.
-   * @return string
-   *   A Base64 encoded representation of the encrypted value.
+   * @param bool $raw_output
+   *   Should be set to TRUE if a raw output value is required. Otherwise, a
+   *   url safe base64 encoded encoded string will be returned.
+   *
+   * @return string|null
+   *   A Base64 encoded representation of the encrypted value or null if
+   *   encryption fails for some reason.
    */
-  public function encrypt($value);
+  public function encrypt($value, $raw_output = FALSE);
 
   /**
    * Decrypt a value using the encryption key from settings.php.
    *
-   * @param $value
-   *   A Base64 encoded representation of an encrypted string.
-   * @return string
-   *   The decrypted value.
+   * @param string $value
+   *   An encrypted string.
+   * @param bool $raw_input
+   *   Should be set to TRUE if the input value is not a base64 encoded/url safe
+   *   string (Defaults to FALSE).
+   *
+   * @return string|null
+   *   The decrypted value or null if decryption fails.
    */
-  public function decrypt($value);
+  public function decrypt($value, $raw_input = FALSE);
 
   /**
    * Gets the `$settings['encryption_key']` value from settings.php.
    *
-   * @return mixed
-   *   The encryption key.
+   * @return string|null
+   *   The encryption key or null if validation fails.
    */
   public function getEncryptionKey();
 
--- src/EncryptionTrait.php
+++ src/EncryptionTrait.php
@@ -6,6 +6,8 @@ use Drupal\Component\Utility\Crypt;
 use Drupal\Core\Site\Settings;
 
 /**
+ * Provides basic encryption/decryption methods.
+ *
  * Used to encrypt and decrypt text using the 'AES-256-CTR' encryption method
  * using the openssl library that in comes with php unless omitted during
  * compilation.
@@ -24,15 +26,17 @@ use Drupal\Core\Site\Settings;
 trait EncryptionTrait {
 
   /**
-   * Encrypt a value using the encryption key from settings.php
+   * Encrypt a value using the encryption key from settings.php.
    *
-   * @param $value
+   * @param string $value
    *   The value tobe encrypted.
    * @param bool $raw_output
    *   Should be set to TRUE if a raw output value is required. Otherwise, a
    *   url safe base64 encoded encoded string will be returned.
-   * @return string
-   *   A Base64 encoded representation of the encrypted value.
+   *
+   * @return string|null
+   *   A Base64 encoded representation of the encrypted value or null if
+   *   encryption fails for some reason.
    */
   public function encrypt($value, $raw_output = FALSE) {
     // Get the encryption key.
@@ -42,11 +46,15 @@ trait EncryptionTrait {
       // Generate a HMAC key using the initialization vector as a salt.
       $h_key = hash_hmac('sha256', hash('sha256', substr($key, 16), TRUE), hash('sha256', substr($iv, 8), TRUE), TRUE);
       // Concatenate the initialization vector and the encrypted value.
-      $cypher = '03'.$iv.openssl_encrypt($value, 'AES-256-CTR', $key, TRUE, $iv);
-      // encode and concatenate the hmac, format code and cypher.
+      $cypher = '03' . $iv . openssl_encrypt($value, 'AES-256-CTR', $key, TRUE, $iv);
+      // Encode and concatenate the hmac, format code and cypher.
       $message = hash_hmac('sha256', $cypher, $h_key, TRUE) . $cypher;
       // Modify the message so it's safe to use in URLs.
-      return $raw_output ? $message : str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($message));
+      return $raw_output ? $message : str_replace(
+        ['+', '/', '='],
+        ['-', '_', ''],
+        base64_encode($message)
+      );
     }
   }
 
@@ -57,15 +65,16 @@ trait EncryptionTrait {
    *   An encrypted string.
    * @param bool $raw_input
    *   Should be set to TRUE if the input value is not a base64 encoded/url safe
-   *   string. (Defaults to FALSE)
-   * @return string
-   *   The decrypted value.
+   *   string (Defaults to FALSE).
+   *
+   * @return string|null
+   *   The decrypted value or null if decryption fails.
    */
   public function decrypt($value, $raw_input = FALSE) {
     // Get the encryption key.
     if ($key = $this->getEncryptionKey()) {
       // Reverse the urls-safe replacement and decode.
-      $message = $raw_input ? $value : base64_decode(str_replace(['-', '_', ''], ['+', '/', '='], $value));
+      $message = $raw_input ? $value : base64_decode(str_replace(['-', '_'], ['+', '/'], $value));
       // Get the cypher hash.
       $hmac = substr($message, 0, 32);
       // Decode the initialization vector.
@@ -82,8 +91,8 @@ trait EncryptionTrait {
   /**
    * Gets the `$settings['encryption_key']` value from settings.php.
    *
-   * @return mixed
-   *   The encryption key.
+   * @return string|null
+   *   The encryption key or null if validation fails.
    */
   public function getEncryptionKey() {
     $key = base64_decode(Settings::get('encryption_key'));
